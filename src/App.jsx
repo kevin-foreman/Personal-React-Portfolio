9a67d2a97e6050466eb174a2a77f9ff41663d01a
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-24 15:12:10 -0500

    Trimmed file structure, and linked in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+
 
 const App = () => {
   return (
     <>
       <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
     </>
   )
 }
