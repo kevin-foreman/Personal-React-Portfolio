a6e4943fdb1214a56bbaad34a363d452480b4c3e
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-24 15:27:51 -0500

    Added general styling for the page, and color schematic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 
 const App = () => {
@@ -14,6 +17,8 @@ const App = () => {
       <About />
       <Experience />
       <Portfolio />
+      <Contact />
+      <Footer />
     </>
   )
 }
