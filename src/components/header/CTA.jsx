1fd6c4dfed03e1c045e654a09cf83ea0a9585651
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-25 11:36:48 -0500

    Added buttons for resume and applied styling

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import resume from '../../assets/resume.pdf'
 
-const callToAction = () => {
+const CTA = () => {
   return (
     <div className='ac'>
-        <a href={resume} download>Download resumé</a>
-        <a href="#contact">Message me</a>
+        <a href={resume} download className='btn btn-primary'>Download my resumé</a>
+        <a href="#contact" className='btn btn-primary'>Click to message me</a>
     </div>
   )
 }
 
-export default callToAction
+export default CTA
