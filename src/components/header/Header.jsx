d6de365f96aa88b52292ea25ba51b4bb471fa1f2
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-25 10:21:31 -0500

    Added some aspects of the header and playing with styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
       <div className="container header__container"></div>
       <h5>Hello, my name is</h5>
       <h1>Kevin Foreman</h1>
+      <h5 className="text-light">Full Stack Developer</h5>
     </header>
   )
 }
