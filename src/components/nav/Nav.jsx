aef0d99e399e9cdfd2a135afc125a66b6badf576
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-26 12:59:04 -0500

    Added some floating Nav bar features

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,7 +9,7 @@ import { AiOutlineContacts } from 'react-icons/ai'
 const Nav = () => {
   return (
     <nav>
-      <a href="#home"><FaHome /></a>
+      <a href="#index" className='active'><FaHome /></a>
       <a href="#about"><SiAboutdotme /></a>
       <a href="#experience"><SiAtom /></a>
       <a href="#portfolio"><AiTwotoneFolderOpen /></a>
