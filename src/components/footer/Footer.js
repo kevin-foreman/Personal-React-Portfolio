aebe88bd8b85393091ff8c6576a85bdc9b19ae71
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-26 20:57:06 -0500

    Added footer information and styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import './footer.css'
+import { FaTwitter } from 'react-icons/fa'
+import { FaTwitch } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="https://masterflex.tech" className='footer__logo'>MasterFlex.Tech</a>
+
+      <ul className='permalinks'>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://twitter.com"><FaTwitter/></a>
+        <a href="https://twitch.com"><FaTwitch/></a>
+        <a href="https://instagram.com"><FaInstagram/></a>
+        <div className='footer__copyright'>
+          <small>&copy; MasterFlex.Tech</small>
+        </div>
+      </div>
+    </footer>
   )
 }
 
